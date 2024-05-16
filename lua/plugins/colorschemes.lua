502d04360268260fc6482adf6cddb7debcdc0fe2
Mcking <sadhwaninaren@gmail.com>
2024-05-15 17:24:02 -0700

    changed colorscheme

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    "sainnhe/sonokai",
+    "sainnhe/gruvbox-material",
+    "sainnhe/everforest",
+    "sainnhe/edge",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      transparent = true,
+      colorscheme = "gruvbox-material",
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+        comments = "italic",
+        keywords = "italic",
+        strings = "italic",
+        variables = "italic",
+      },
+    },
+  },
+}
